6697cab77a4ab25249bfe06fb61fe13bd0f2d81f
LermithB <lermith17@gmail.com>
2021-10-27 14:51:20 -0600

    Tabla Evaluacion Completa

--- Script/Matricula.sql
+++ Script/Matricula.sql
@@ -1,22 +1,22 @@
---Lermith Biarreta Portillo y Adrián Herrera Segura
+-- Lermith Biarreta Portillo y Adrián Herrera Segura
 
---Base de datos que administra la información de un colegio
---Matriculas, mensualidades,  horarios, clases, profesores, etc.
+-- Base de datos que administra la información de un colegio
+-- Matriculas, mensualidades,  horarios, clases, profesores, etc.
 
 use master
 go
 ALTER DATABASE matricula set single_user with rollback immediate
 go
-DROP DATABASE IF EXISTS matricula 
+DROP DATABASE IF EXISTS matricula;
 go
 create database matricula;
 go
 use matricula;
 go
 
---Creación de tablas
+-- Creación de tablas
 
---Tabla que posee la información general que posee un usuario registrado en la base de datos.
+-- Tabla que posee la información general que posee un usuario registrado en la base de datos.
 
 create table usuario
 (
@@ -32,18 +32,18 @@ create table usuario
 	PRIMARY KEY (cedula)
 );
 
---Tabla que posee la información de un usuario de categoría profesor.
+-- Tabla que posee la información de un usuario de categoría profesor.
 
 create table profesor
 (
 	cedulaProfesor int,
 	materiaImpartida varchar(30) NOT NULL,
-	salario money NOT NULL,
+	salario int NOT NULL,
 	PRIMARY KEY (cedulaProfesor),
 	FOREIGN KEY (cedulaProfesor) REFERENCES usuario(cedula)
 );
 
---Tabla que posee la información del grupo que va a manejar el profesor a cargo.
+-- Tabla que posee la información del grupo que va a manejar el profesor a cargo.
 
 create table grupo
 (
@@ -59,7 +59,7 @@ create table grupo
 	FOREIGN KEY (cedulaProfesor) REFERENCES profesor(cedulaProfesor)
 );
 
---Tabla que posee la información de un usuario de categoría estudiante.
+-- Tabla que posee la información de un usuario de categoría estudiante.
 
 create table estudiante
 (
@@ -74,7 +74,7 @@ create table estudiante
 	FOREIGN KEY (grupo) REFERENCES grupo(codigoGrupo)
 );
 
---Tabla que posee la información del periodo lectivo en el que se encuentra el estudiante.
+-- Tabla que posee la información del periodo lectivo en el que se encuentra el estudiante.
 
 create table periodoLectivo
 (
@@ -89,7 +89,7 @@ create table periodoLectivo
 	FOREIGN KEY (cedulaEstudiante) REFERENCES estudiante(cedulaEstudiante)
 );
 
---Tabla que posee la información del horario de clases de cada estudiante.
+-- Tabla que posee la información del horario de clases de cada estudiante.
 
 create table horarioClases
 (
@@ -105,17 +105,19 @@ create table horarioClases
 
 );
 
---Tabla de posee la tabla de evaluaciones de los cursos del colegio
+-- Tabla de posee la tabla de evaluaciones de los cursos del colegio
 
-create table evaluaciones
+create table evaluacion
 (
+	codigoEvaluacion int NOT NULL, 
 	evaluacion varchar(30) NOT NULL,
 	porcentaje int NOT NULL CHECK (porcentaje BETWEEN 1 AND 100),
 	curso int,
+	PRIMARY KEY (codigoEvaluacion),
 	FOREIGN KEY (curso) REFERENCES horarioClases(CodigoCurso)
 );
 
---Tabla que posee la información de un usuario de categoría padre.
+-- Tabla que posee la información de un usuario de categoría padre.
 
 create table padre
 (
@@ -123,13 +125,13 @@ create table padre
 	profesion varchar(30) NOT NULL,
 	conyugue varchar(60),
 	telefonoConyugue varchar(8),
-	costoMensualidad money NOT NULL,
+	costoMensualidad int NOT NULL,
 	pagosRealizados int NOT NULL,
 	PRIMARY KEY (cedulaPadre),
 	FOREIGN KEY (cedulaPadre) REFERENCES usuario(cedula)
 );
 
---Tabla que posee la información de la matricula de cada estudiante del colegio.
+-- Tabla que posee la información de la matricula de cada estudiante del colegio.
 
 create table matricula
 (
@@ -137,14 +139,14 @@ create table matricula
 	periodoMatricular int NOT NULL CHECK (periodoMatricular > 0),
 	año int NOT NULL CHECK (año > 0),
 	cobrosPendientes int NOT NULL CHECK (cobrosPendientes > 0),
-	montoMatricula money NOT NULL,
+	montoMatricula int NOT NULL,
 	profesionPadre varchar(30),
 	cedulaPadre int,
 	PRIMARY KEY(cedulaEstudiante, periodoMatricular, año),
 	FOREIGN KEY (cedulaPadre) REFERENCES padre(cedulaPadre)
 );
 
---Tabla con la información que debe tener la factura que se le hace al padre al pagar la matrícula.
+-- Tabla con la información que debe tener la factura que se le hace al padre al pagar la matrícula.
 
 create table factura
 (
@@ -152,42 +154,38 @@ create table factura
 	periodo int NOT NULL,
 	año int NOT NULL,
 	fechaPago date NOT NULL,
-	montoTotal money NOT NULL,
+	montoTotal int NOT NULL,
 	cedulaEstudiante int NOT NULL,
 	PRIMARY KEY (cedulaPadre),
 	FOREIGN KEY (cedulaEstudiante, periodo, año) REFERENCES matricula(cedulaEstudiante, periodoMatricular, año)
 );
 
----------------------------Creación de Vistas--------------------------------------------------
+-- ------------------------- Creación de Vistas --------------------------------------------------;
 
 GO
-
-CREATE VIEW informacionEvaluaciones as --Lanza información general de las evaluaciones (Profesores)
+CREATE VIEW informacionEvaluaciones as -- Lanza información general de las evaluaciones (Profesores)
 	(SELECT evaluacion, porcentaje FROM [dbo].[evaluaciones], [dbo].[horarioClases]
 		WHERE CodigoCurso = curso)
 
 GO
-
-CREATE VIEW controlCobrosAdeudados as --Lanza información de los pagos necesarios por estudiante (Padres)
+CREATE VIEW controlCobrosAdeudados as -- Lanza información de los pagos necesarios por estudiante (Padres)
 	(SELECT cobrosPendientes, montoMatricula, costoMensualidad, pagosRealizados FROM [dbo].[matricula], [dbo].[padre]
 		WHERE [dbo].[padre].[cedulaPadre] = [dbo].[matricula].[cedulaPadre])
 
 GO
-
-CREATE VIEW informacionEstudiante as --Lanza información general de los estudiantes (padres)
+CREATE VIEW informacionEstudiante as -- Lanza información general de los estudiantes (padres)
 	(SELECT [dbo].[estudiante].[cedulaEstudiante], gradosCursados, grupo, cursoActual FROM [dbo].[estudiante], [dbo].[matricula]
 		WHERE [dbo].[estudiante].[cedulaEstudiante] = [dbo].[matricula].[cedulaEstudiante])
 
 GO
-
-CREATE VIEW informacionGradoEstudiante as --Lanza información general del grado cursado (estudiantes)
+CREATE VIEW informacionGradoEstudiante as -- Lanza información general del grado cursado (estudiantes)
 	(SELECT gradosCursados, grupo, periodo, cursoActual FROM [dbo].[estudiante], [dbo].[matricula]
 		WHERE [dbo].[estudiante].[cedulaEstudiante] = [dbo].[matricula].[cedulaEstudiante])
 
 GO
-
-CREATE VIEW informacionCurso as --Lanza información general del curso y el horario del estudiante (estudiantes)
+CREATE VIEW informacionCurso as -- Lanza información general del curso y el horario del estudiante (estudiantes)
 	(SELECT CodigoCurso, NombreCurso, aula, cedulaProfesor  FROM [dbo].[horarioClases], [dbo].[grupo]
 	 WHERE materia = NombreCurso)
+
 
 
